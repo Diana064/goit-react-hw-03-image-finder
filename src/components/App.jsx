42b7ca9c12fa6897be33ea5757b7fc7dc9af0ec2
Diana064 <dipustolyakova02@gmail.com>
2023-02-05 13:07:46 +0200

    change state try to fix load more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,114 @@
+/* eslint-disable no-sequences */
 import React, { Component } from 'react';
-
+import { Loader } from './loader/Loader';
 import { Searchbar } from './searchbar/Searchbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { ImageGallery } from './imageGallery/ImageGallery';
+import ImageGallery from './imageGallery/ImageGallery';
 import css from './App.module.css';
+import { Button } from './button/Button';
+
+const API_KEY = '31465649-f1ff204e289e0f72e30576924';
+const BASE_URL = 'https://pixabay.com/api/?';
 
 export class App extends Component {
   state = {
+    status: 'idle',
+    page: 1,
     pictureName: '',
+    images: null,
+    showModal: false,
+    largeImageURL: '',
+    error: null,
+    showButton: true,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { page, pictureName } = this.state;
+
+    if (pictureName !== prevState.pictureName) {
+      this.setState({ page: 1, status: 'pending', images: [] });
+      this.getImages(page, pictureName);
+      return;
+    }
+    if (page > prevState.page) {
+      this.getImages(page, pictureName);
+      return;
+    }
+  }
+  getImages = (page, pictureName) => {
+    fetch(
+      `${BASE_URL}q=${pictureName}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(
+          new Error(`Something wrong with this request ${pictureName}`)
+        );
+      })
+      .then(({ hits }) => {
+        const imagesList = hits.map(
+          ({ id, webformatURL, largeImageURL, tags }) => {
+            return {
+              id,
+              webformatURL,
+              largeImageURL,
+              tags,
+            };
+          }
+        );
+        return imagesList;
+      })
+      .then(imagesList => {
+        this.setState(({ images }) => ({
+          images: [...images, ...imagesList],
+          status: 'resolved',
+          // showButton: this.state.page < Math.ceil(totalHits / 12),
+        }));
+      })
+      .catch(error => this.setState({ error, status: 'rejected' }));
+    console.log(this.state.showButton);
   };
 
   onSubmitFormHandler = pictureName => {
     if (pictureName) {
       this.setState({ pictureName });
     }
+    console.log(this.state.showButton);
   };
+
+  loadMore = () => {
+    this.setState(({ page }) => ({
+      page: page + 1,
+    }));
+  };
+
   render() {
-    const { pictureName } = this.state;
+    const { images, status, error } = this.state;
+    if (status === 'pending') {
+      return <Loader />;
+    }
+    if (status === 'rejected') {
+      return <div>{error.message}</div>;
+    }
+    if (status === 'resolved') {
+      return (
+        <div className={css.App}>
+          <Searchbar onSubmit={this.onSubmitFormHandler} />
+          <ImageGallery images={images} />
+
+          <Button onClick={this.loadMore}>Load More</Button>
+        </div>
+      );
+    }
+
     return (
       <div className={css.App}>
         <Searchbar onSubmit={this.onSubmitFormHandler} />
-        <ImageGallery pictureName={pictureName} />
         <ToastContainer />
-        {/* Same as */}
+        {/* {showButton && <Button onClick={this.loadMore}>Load More</Button>} */}
       </div>
     );
   }
